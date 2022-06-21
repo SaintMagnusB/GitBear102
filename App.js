c4865418db55ab13d3bf77651dd1df800258e18c
SaintMagnus <magnusbjarniska@gmail.com>
2022-06-21 08:08:20 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='nightmode'>
+      <div className="App">Website layout:
+        <h1>Hello world! This is my template</h1>
+        <br />
+        <div>contact information</div>
+        <div>Email: magnusbska@</div>
+        <br />
+        <div>little bit about me, maybe an image as well</div>
+        <br />
+        <div>skills related to front end development</div>
+        <br />
+        <div>connect info</div>
+        <br />
+        <div>footer with information</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ information.js
@@ -0,0 +1,8 @@
+function contactInfo() {
+    let phone = '(+354) 784-9845';
+    let email = 'magnusbska@';
+}
+
+function aboutMe() {
+    let 
+}
